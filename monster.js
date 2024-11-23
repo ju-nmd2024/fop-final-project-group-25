3f2760ca1caa7fe0d2bc8a39afbe96fbccd8639a
Zora Pintér <pizo23pq@student.ju.se>
2024-11-23 15:06:08 +0100

    monster.js
    basic let and right movement of the soon to be monster

--- monster.js
+++ monster.js
@@ -1,6 +1,16 @@
-class Monster {
-    constructor(){
-        
-    }
-
+let x = 50; 
+let speed = 3;
+function setup() {
+  createCanvas(600, 400);	
 }
+function draw() {
+	background(255);
+	fill(0);
+	ellipse(x, 50, 50, 50);
+	x = x + speed;
+		if(x + 25 >= width || x - 25 <= 0)
+	{
+		speed = -speed;
+	}
+}
+
